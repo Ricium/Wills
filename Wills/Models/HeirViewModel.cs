ee0f7df879465956d004721a90b006e6699ba9f3
Christo Erasmu <ice1@me.com>
2017-02-12 16:17:40 +0200

    ViewModels

--- Wills/Models/HeirViewModel.cs
+++ Wills/Models/HeirViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +9,38 @@ namespace Wills.Models
 {
     public class HeirViewModel
     {
+        [Required]
+        [DisplayName("Heir ID")]
         public int HeirID { get; set; }
+        [Required]
+        [DisplayName("Full Will ID")]
         public int FullWillID { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        [StringLength(50)]
         public string Name { get; set; }
+        [Required]
+        [DisplayName("Surname")]
+        [StringLength(50)]
         public string Surname { get; set; }
+        [Required]
+        [DisplayName("ID Number")]
+        [StringLength(13)]
         public string IDNumber { get; set; }
+        [Required]
+        [DisplayName("Relationship ID")]
         public int RelationshipID { get; set; }
+        [Required]
+        [DisplayName("Is Heirs")]
         public bool IsHeir { get; set; }
+      
+        [DisplayName("Heir Percentage")]
         public Nullable<decimal> HeirPercentage { get; set; }
+        [DisplayName("Bequest Amount")]
+     
         public Nullable<decimal> BequestAmount { get; set; }
+        [DisplayName("Bequest Item")]
+        [StringLength(250)]
         public string BequestItem { get; set; }
     }
 }
--- Wills/Models/WillViewModel.cs
+++ Wills/Models/WillViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,29 +9,76 @@ namespace Wills.Models
 {
     public class WillViewModel
     {
+        [Required]
+        [DisplayName("Will ID")]
         public int WillID { get; set; }
+        [Required]
+        [DisplayName("User ID")]
         public string UserID { get; set; }
+        [Required]
+        [DisplayName("Name")]
+        [StringLength(50)]
         public string PrimaryClientName { get; set; }
+        [Required]
+        [DisplayName("Surname")]
+        [StringLength(50)]
         public string PrimaryClientSurname { get; set; }
+        [Required]
+        [DisplayName("ID Number")]
+        [StringLength(13)]
         public string PrimaryClientIDNumber { get; set; }
+
+        [DisplayName("Name")]
+        [StringLength(50)]
         public string SecandaryClientName { get; set; }
+
+        [DisplayName("Surname")]
+        [StringLength(50)]
         public string SecanderyClientSurname { get; set; }
+        [DisplayName("ID Number")]
+        [StringLength(13)]
         public string SecandaryClientIDNumber { get; set; }
+        [DisplayName("Will Type")]
         public int WillTypeID { get; set; }
+        [DisplayName("Marital Status")]
         public int MaritalStatusID { get; set; }
+        [DisplayName("Name")]
+        [StringLength(50)]
         public string GardianName { get; set; }
+        [DisplayName("Surname")]
+        [StringLength(50)]
         public string GardianSurname { get; set; }
+        [DisplayName("ID Number")]
+        [StringLength(13)]
         public string GardianIDNumber { get; set; }
+        [DisplayName("Name")]
+        [StringLength(50)]
         public string CoExecutorName { get; set; }
+        [DisplayName("Surname")]
+        [StringLength(50)]
         public string CoExecutorSurname { get; set; }
+        [DisplayName("ID Number")]
+        [StringLength(50)]
         public string CoExecutorIDNumber { get; set; }
+        [DisplayName("Name")]
+        [StringLength(50)]
         public string CoTrusteeName { get; set; }
+        [DisplayName("Surname")]
+        [StringLength(50)]
         public string CoTrusteeSurname { get; set; }
+        [DisplayName("ID Number")]
+        [StringLength(13)]
         public string CoTrusteeIDNumber { get; set; }
+        [DisplayName("Would you like your remains to be cremated?")]
         public bool DoCremation { get; set; }
+        [DisplayName("Would you like to provide for a living will?")]
         public bool IsLivingWill { get; set; }
+        [DisplayName("Would you like your organs donated for medical research and/or transplantpurposes?")]
         public bool IsOrganDoner { get; set; }
+        [DisplayName("Specify your own request.")]
         public bool HasSpecialRequest { get; set; }
+        [DisplayName("Special Request")]
+        [StringLength(50)]
         public string SpecialRequest { get; set; }
 
         List<HeirViewModel> Heirs { get; set; }
